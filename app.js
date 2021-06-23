fbdb59a340cdd129e26346b056a4ae786e0e4e2b
eperdomo09 <eraldoepl@gmail.com>
2021-06-23 16:01:55 -0400

    moving CORS function.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 require("./startup/logging").winstonHandlers();
+require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
--- startup/prod.js
+++ startup/prod.js
@@ -1,9 +1,7 @@
 const helmet = require("helmet");
 const compression = require("compression");
-const cors = require("cors");
 
 module.exports = function (app) {
   app.use(helmet());
   app.use(compression());
-  if (process.env.NODE_ENV === "development") app.use(cors());
 };
